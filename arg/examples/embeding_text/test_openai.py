cd37ce96b455bd8b7597cf2d37143ec1dce5e2e3
Anran Fan <anran.fan@shopee.com>
2026-05-10 18:00:46 +0800

    first commit

--- arg/examples/embeding_text/test_openai.py
+++ arg/examples/embeding_text/test_openai.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="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")
+    api_key="")
 
 response = client.responses.create(
     model="gpt-4.1-mini",
